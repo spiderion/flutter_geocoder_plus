a0b231d102436fc6a10bff4925ea5f661cb79ef9
IonOlaru89 <ion.olaru@projectolympus.co.za>
2025-04-01 20:17:19 +0200

    fixed

--- android/src/main/java/com/aloisdeniel/geocoder/GeocoderPlugin.java
+++ android/src/main/java/com/aloisdeniel/geocoder/GeocoderPlugin.java
@@ -1,233 +1,132 @@
 package com.aloisdeniel.geocoder;
 
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.HashMap;
-import java.io.IOException;
-import java.lang.Exception;
 import android.content.Context;
 import android.location.Address;
 import android.location.Geocoder;
-import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.Looper;
 
+import androidx.annotation.NonNull;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.MethodCall;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
-/**
- * NotAvailableException
- */
-class NotAvailableException extends Exception {
-  NotAvailableException() {}
-}
+public class GeocoderPlugin implements FlutterPlugin, MethodCallHandler {
 
-/**
- * GeocoderPlugin
- */
-public class GeocoderPlugin implements MethodCallHandler {
+    private MethodChannel channel;
+    private Geocoder geocoder;
+    private Context context;
 
-  private Geocoder geocoder;
-
-  public GeocoderPlugin(Context context) {
-
-    this.geocoder = new Geocoder(context);
-  }
-
-  /**
-   * Plugin registration.
-   */
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "github.com/aloisdeniel/geocoder");
-    channel.setMethodCallHandler(new GeocoderPlugin(registrar.context()));
-  }
-
-  // MethodChannel.Result wrapper that responds on the platform thread.
-  private static class MethodResultWrapper implements Result {
-    private Result methodResult;
-    private Handler handler;
-
-    MethodResultWrapper(Result result) {
-      methodResult = result;
-      handler = new Handler(Looper.getMainLooper());
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
+        context = binding.getApplicationContext();
+        geocoder = new Geocoder(context);
+        channel = new MethodChannel(binding.getBinaryMessenger(), "github.com/aloisdeniel/geocoder");
+        channel.setMethodCallHandler(this);
     }
 
     @Override
-    public void success(final Object result) {
-      handler.post(
-          new Runnable() {
-            @Override
-            public void run() {
-              methodResult.success(result);
-            }
-          });
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        channel.setMethodCallHandler(null);
+        channel = null;
+        geocoder = null;
     }
 
     @Override
-    public void error(
-        final String errorCode, final String errorMessage, final Object errorDetails) {
-      handler.post(
-          new Runnable() {
-            @Override
-            public void run() {
-              methodResult.error(errorCode, errorMessage, errorDetails);
-            }
-          });
+    public void onMethodCall(MethodCall call, Result result) {
+        if (call.method.equals("findAddressesFromQuery")) {
+            String address = call.argument("address");
+            findAddressesFromQuery(address, result);
+        } else if (call.method.equals("findAddressesFromCoordinates")) {
+            double latitude = ((Number) call.argument("latitude")).doubleValue();
+            double longitude = ((Number) call.argument("longitude")).doubleValue();
+            findAddressesFromCoordinates(latitude, longitude, result);
+        } else {
+            result.notImplemented();
+        }
     }
 
-    @Override
-    public void notImplemented() {
-      handler.post(
-          new Runnable() {
-            @Override
-            public void run() {
-              methodResult.notImplemented();
-            }
-          });
-    }
-  }
-
-  @Override
-  public void onMethodCall(MethodCall call, Result rawResult) {
-    Result result = new MethodResultWrapper(rawResult);
-
-    if (call.method.equals("findAddressesFromQuery")) {
-      String address = (String) call.argument("address");
-      findAddressesFromQuery(address, result);
-    }
-    else if (call.method.equals("findAddressesFromCoordinates")) {
-      float latitude = ((Number) call.argument("latitude")).floatValue();
-      float longitude = ((Number) call.argument("longitude")).floatValue();
-      findAddressesFromCoordinates(latitude,longitude, result);
-    } else {
-      result.notImplemented();
-    }
-  }
-
-  private void assertPresent() throws NotAvailableException {
-    if (!geocoder.isPresent()) {
-      throw new NotAvailableException();
-    }
-  }
-
-  private void findAddressesFromQuery(final String address, final Result result) {
-
-    final GeocoderPlugin plugin = this;
-    new AsyncTask<Void, Void, List<Address>>() {
-        @Override
-        protected List<Address> doInBackground(Void... params) {
+    private void findAddressesFromQuery(final String address, final Result result) {
+        new Thread(() -> {
             try {
-                plugin.assertPresent();
-                return geocoder.getFromLocationName(address, 20);
-            } catch (IOException ex) {
-                return null;
-            } catch (NotAvailableException ex) {
-                return new ArrayList<>();
+                List<Address> addresses = geocoder.getFromLocationName(address, 20);
+                postResult(result, createAddressMapList(addresses));
+            } catch (IOException e) {
+                postError(result, "io_error", e.getMessage());
             }
-        }
+        }).start();
+    }
 
-        @Override
-        protected void onPostExecute(List<Address> addresses) {
-            if (addresses != null) {
-                if (addresses.isEmpty())
-                    result.error("not_available", "Empty", null);
-
-                else result.success(createAddressMapList(addresses));
-            }
-            else result.error("failed", "Failed", null);
-        }
-    }.execute();
-  }
-
-  private void findAddressesFromCoordinates(final float latitude, final float longitude, final Result result) {
-    final GeocoderPlugin plugin = this;
-    new AsyncTask<Void, Void, List<Address>>() {
-        @Override
-        protected List<Address> doInBackground(Void... params) {
+    private void findAddressesFromCoordinates(final double latitude, final double longitude, final Result result) {
+        new Thread(() -> {
             try {
-                plugin.assertPresent();
-                return geocoder.getFromLocation(latitude, longitude, 20);
-            } catch (IOException ex) {
-                return null;
-            } catch (NotAvailableException ex) {
-                return new ArrayList<>();
+                List<Address> addresses = geocoder.getFromLocation(latitude, longitude, 20);
+                postResult(result, createAddressMapList(addresses));
+            } catch (IOException e) {
+                postError(result, "io_error", e.getMessage());
             }
-        }
-
-        @Override
-        protected void onPostExecute(List<Address> addresses) {
-            if (addresses != null) {
-                if (addresses.isEmpty())
-                    result.error("not_available", "Empty", null);
-
-                else result.success(createAddressMapList(addresses));
-            }
-            else result.error("failed", "Failed", null);
-        }
-    }.execute();
-  }
-
-  private Map<String, Object> createCoordinatesMap(Address address) {
-
-    if(address == null)
-      return null;
-
-    Map<String, Object> result = new HashMap<String, Object>();
-
-    result.put("latitude", address.getLatitude());
-    result.put("longitude", address.getLongitude());
-
-    return result;
-  }
-
-  private Map<String, Object> createAddressMap(Address address) {
-
-    if(address == null)
-      return null;
-
-    // Creating formatted address
-    StringBuilder sb = new StringBuilder();
-    for (int i = 0; i <= address.getMaxAddressLineIndex(); i++) {
-      if (i > 0) {
-        sb.append(", ");
-      }
-      sb.append(address.getAddressLine(i));
+        }).start();
     }
 
-    Map<String, Object> result = new HashMap<String, Object>();
-
-    result.put("coordinates", createCoordinatesMap(address));
-    result.put("featureName", address.getFeatureName());
-    result.put("countryName", address.getCountryName());
-    result.put("countryCode", address.getCountryCode());
-    result.put("locality", address.getLocality());
-    result.put("subLocality", address.getSubLocality());
-    result.put("thoroughfare", address.getThoroughfare());
-    result.put("subThoroughfare", address.getSubThoroughfare());
-    result.put("adminArea", address.getAdminArea());
-    result.put("subAdminArea", address.getSubAdminArea());
-    result.put("addressLine", sb.toString());
-    result.put("postalCode", address.getPostalCode());
-
-    return result;
-  }
-
-  private List<Map<String, Object>> createAddressMapList(List<Address> addresses) {
-
-    if(addresses == null)
-      return new ArrayList<Map<String, Object>>();
-
-    List<Map<String, Object>> result = new ArrayList<Map<String, Object>>(addresses.size());
-
-    for (Address address : addresses) {
-      result.add(createAddressMap(address));
+    private void postResult(Result result, Object value) {
+        new Handler(Looper.getMainLooper()).post(() -> result.success(value));
     }
 
-    return result;
-  }
+    private void postError(Result result, String code, String message) {
+        new Handler(Looper.getMainLooper()).post(() -> result.error(code, message, null));
+    }
+
+    private Map<String, Object> createCoordinatesMap(Address address) {
+        if (address == null) return null;
+        Map<String, Object> result = new HashMap<>();
+        result.put("latitude", address.getLatitude());
+        result.put("longitude", address.getLongitude());
+        return result;
+    }
+
+    private Map<String, Object> createAddressMap(Address address) {
+        if (address == null) return null;
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i <= address.getMaxAddressLineIndex(); i++) {
+            if (i > 0) sb.append(", ");
+            sb.append(address.getAddressLine(i));
+        }
+
+        Map<String, Object> result = new HashMap<>();
+        result.put("coordinates", createCoordinatesMap(address));
+        result.put("featureName", address.getFeatureName());
+        result.put("countryName", address.getCountryName());
+        result.put("countryCode", address.getCountryCode());
+        result.put("locality", address.getLocality());
+        result.put("subLocality", address.getSubLocality());
+        result.put("thoroughfare", address.getThoroughfare());
+        result.put("subThoroughfare", address.getSubThoroughfare());
+        result.put("adminArea", address.getAdminArea());
+        result.put("subAdminArea", address.getSubAdminArea());
+        result.put("addressLine", sb.toString());
+        result.put("postalCode", address.getPostalCode());
+
+        return result;
+    }
+
+    private List<Map<String, Object>> createAddressMapList(List<Address> addresses) {
+        List<Map<String, Object>> result = new ArrayList<>();
+        if (addresses == null) return result;
+
+        for (Address address : addresses) {
+            result.add(createAddressMap(address));
+        }
+
+        return result;
+    }
 }
-
--- example/android/app/src/main/kotlin/com/example/example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/example/example/MainActivity.kt
@@ -1,6 +1,5 @@
-package com.example.example
+package com.example.flutter_geocoder_plus_example
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity()
